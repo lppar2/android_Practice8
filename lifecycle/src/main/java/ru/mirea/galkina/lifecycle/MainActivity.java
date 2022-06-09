76ab480b7fc49349007b967ce81549987db8c0e3
Valeria Galkina <lppar2@gmail.com>
2022-06-09 03:05:11 +0300

    Initial commit

---
+++ lifecycle/src/main/java/ru/mirea/galkina/lifecycle/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.mirea.galkina.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private Server server;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        server = new Server();
+        getLifecycle().addObserver(server);
+    }
+}
---
+++ livedata/src/main/java/ru/mirea/galkina/livedata/TimeLiveData.java
@@ -0,0 +1,32 @@
+package ru.mirea.galkina.livedata;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TimeLiveData {
+    private static MutableLiveData<Long> data = new MutableLiveData<Long>();
+
+    static LiveData<Long> getTime(){
+        data.setValue(new Date().getTime());
+        return data;
+    }
+    static void setTime(){
+        data.setValue(new Date().getTime());
+    }
+
+
+    private static LiveData<String> getStringTime = Transformations.map(data, input -> {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format(calendar.getTime());
+    });
+
+    static LiveData<String> getDate(){
+        return getStringTime;
+    }
+}
